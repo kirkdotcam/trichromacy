f9e79bbc7aadfcddeedcf8544ad0471b4bd26c11
camkirk <camden.kirkland@gmail.com>
2018-04-09 00:28:03 -0500

    Adding base functionality

---
+++ public/js/main.js
@@ -0,0 +1,56 @@
+var colorWheel = ['red', 'blue', 'green'];
+$(document).ready(function () {
+  var $colors = $('#colorDiv');
+  //delegate colorAjax() to colorDiv
+  colorGen()
+
+  $('.colorBox').click(function(){
+    var colorData = {}
+
+    $colors.children('div').each(function(i,el){
+      var toDecompose = $(this).css('background-color');
+      var decomposed = [];
+
+      toDecompose.substring(4,(toDecompose.length-2)).split(',').forEach(function(el){
+        decomposed.push(el)
+      });
+      colorData['box_' + i] = decomposed;
+      console.log(Object.keys(colorData));
+    });
+
+    colorData.goal = $('#goalColor').data('newColor');
+    console.log(colorData)
+
+
+    $.post('/colors',colorData)
+    .done(function(data){
+      console.log(`data submitted `,data);
+    })
+  })
+
+  function colorGen() {
+    $colors.empty()
+    var text = $('<h3>');
+    var newColor = colorWheel[Math.floor(Math.random() * 2) + 1]
+    text.text(`Which color has the most ${newColor}?`);
+    text.attr('id','goalColor');
+    text.data('newColor',newColor);
+
+    $colors.append(text);
+
+    for (var i = 0; i < 3; i++) {
+      console.log('gen div')
+      var genDiv = $('<div>')
+      genDiv.css("background-color", function () {
+        return `rgb(${rand256()},${rand256()},${rand256()})`
+      });
+      genDiv.attr('id', i);
+      genDiv.attr('class', 'colorBox');
+      $colors.append(genDiv);
+    }
+
+  }
+})
+function rand256() {
+  return Math.floor(Math.random() * 255) + 1;
+}
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-var bc = require('badcube');
-var tf = require('@tensorflow/tfjs');
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-var app = express();
+let bc = require('badcube');
+let tf = require('@tensorflow/tfjs');
+let express = require('express');
+let bodyParser = require('body-parser');
+let path = require('path');
+let app = express();
 
 app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-
 app.get('/',function(req,res){
 	res.sendFile('./index.html');
 });
+
+app.post('/colors',function(req,res){
+	Submissions.insert(req.body)
+	console.log(req.body)
+	res.end()
+})
 
 app.listen(3000,function(){
 	console.log('listening on port 3000');
