744de7ea3822fde02642535f3a1d9641c367217f
CamKirk <camden.kirkland@gmail.com>
2019-07-03 11:07:59 -0500

    shapes fixed but printing NaN

--- utils/predictions.js
+++ utils/predictions.js
@@ -1,30 +1,34 @@
+// let tf = require('@tensorflow/tfjs-node-gpu');
 let tf = require('@tensorflow/tfjs');
 let mongojs = require('mongojs')
-
-
 // model setup
 let model = tf.sequential();
 
 model.add(tf.layers.dense({
+    inputShape:[2],
     units:6,
-    inputShape:[3],
     activation:'relu'
 }))
 
 model.add(tf.layers.dense({
     units:6,
+    activation:'relu'
+}))
+
+model.add(tf.layers.dense({
+    units:1,
     activation:'relu'
 }))
 
 model.compile({
     optimizer: 'sgd',
-    loss:'categoricalCrossentropy',
+    loss:'meanSquaredError',
     metrics:['accuracy']
 })
-
+// model.summary();
 
 // retrieve data through mongo
-db=mongojs('colors',['submissions'])
+let db=mongojs('colors',['submissions'])
 
 db.on('connect',()=> console.log("database connected"));
 db.on('error', (err) => console.error(err));
@@ -33,8 +37,24 @@ let submissionCollection = db.collection('submissions');
 
 submissionCollection.find(function(err, docs){
     // console.log(docs);
-    data = docs.map((doc) => [tf.tensor(doc.choices), doc.bias, doc.correct])
-    console.log(data);
-    // model.fit
-    // print accuracy
+    let data = docs.map((doc) => [tf.tensor(doc.choices),tf.tensor(doc.bias), tf.tensor1d([+doc.correct])]);
+
+    let Xs = data.map((observation) => [observation[0], observation[1]]);
+    let ys = data.map((observation) => observation[2]);
+    console.log(Xs[0])
+    
+    
+    model
+        .fit(tf.stack(Xs),tf.stack(ys),{
+            batchSize:2
+        })
+        .catch(err => console.log(err))
+        .then(()=>{
+
+            let newdata =[tf.tensor([[31,83,163],[138,77,157],[199,131,60]]),tf.tensor([6,9,13])]
+            console.log(tf.tensor(newdata,[1,2]));
+            
+            model.predict(tf.tensor(newdata,[1,2])).print()
+        })
+
 })
