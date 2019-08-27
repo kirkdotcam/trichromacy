b537502bf513efb5b2281ad012c0424778af27a4
CamKirk <camden.kirkland@gmail.com>
2019-08-27 18:56:36 -0500

    creates working prediction

--- utils/predictions.js
+++ utils/predictions.js
@@ -4,30 +4,6 @@ let mongojs = require('mongojs')
 // model setup
 let model = tf.sequential();
 
-model.add(tf.layers.dense({
-    inputShape:[3,3],
-    batchSize:19, //number of samples to use?
-    units:6,
-    activation:'linear'
-}))
-
-model.add(tf.layers.dense({
-    units:6,
-    activation:'linear'
-}))
-
-model.add(tf.layers.dense({
-    units:1,
-    activation:'linear'
-}))
-
-model.compile({
-    optimizer: 'sgd',
-    loss:'meanSquaredError',
-    metrics:['accuracy']
-})
-model.summary();
-
 // retrieve data through mongo
 let db=mongojs('colors',['submissions'])
 
@@ -37,30 +13,60 @@ db.on('error', (err) => console.error(err));
 let submissionCollection = db.collection('submissions');
 
 submissionCollection.find(function(err, docs){
-    // console.log(docs);
-    // TODO: Let's vectorize each individual choice (or color) instead of keeping all one tensor
 
-    let data = docs.map((doc) => [doc.choices,+doc.correct]);
+    modelsetup(docs.length)
+    // console.log(docs);
+
+    let data = docs.map((doc) => [doc.choices, Number(doc.correct)]);
     let Xs = tf.tensor(data.map((observation) => observation[0]));
-    let ys = tf.tensor(data.map((observation) => observation[1]));
-    Xs.print()
-    ys.print()
-    // tf.stack(Xs).print()
- 
+    let ys = tf.tensor(data.map((observation) => [[observation[1]],[observation[1]],[observation[1]]]));
+    // let ys = tf.randomNormal([19,3,1])
+    // Xs.print()
+    // ys.print()
+
 
     model
         .fit(Xs,ys,{
-        // .fit(undata,labels,{
-            batchSize:19
+            batchSize:docs.length
         })
         .catch(err => console.log("my error",err))
         .then((info)=>{
+
             console.log(info.history);
             
-            // let newdata =[tf.tensor([[31,83,163],[138,77,157],[199,131,60]])]
-            // console.log(tf.tensor(newdata));
+            let newdata =tf.tensor([[[31,83,163],[138,77,157],[199,131,60]]]);
+            // console.log(newdata);
             
-            // model.predict(tf.tensor(newdata,[1,2])).print()
+            console.log("newprediction");
+            
+            model.predict(newdata).print()
         })
 
 })
+
+function modelsetup(batchSize) {
+
+    model.add(tf.layers.dense({
+        inputShape:[3,3],
+        batchSize:batchSize, //number of samples to use?
+        units:3,
+        activation:'softmax'
+    }))
+    
+    model.add(tf.layers.dense({
+        units:3,
+        activation:'sigmoid'
+    }))
+    
+    model.add(tf.layers.dense({
+        units:1,
+        activation:"sigmoid"
+    }))
+    
+    model.compile({
+        optimizer: 'sgd',
+        loss:'meanSquaredError',
+        metrics:['accuracy']
+    })
+    // model.summary();
+}
